970be08b53b87b94f4487eb8224bf23364ace8ae
Thada Wangthammang <mildronize@gmail.com>
2020-05-26 01:09:06 +0700

    add react-bootstrap

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import { AppProps } from 'next/app';
+
+import '../styles/bootstrap.scss';
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps } : AppProps) {
+  return <Component {...pageProps} />
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
+import { Button } from 'react-bootstrap';
+
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+    <>
+  <Button variant="primary">Primary</Button>{' '}
+  <Button variant="secondary">Secondary</Button>{' '}
+  <Button variant="success">Success</Button>{' '}
+  <Button variant="warning">Warning</Button>{' '}
+  <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+  <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+  <Button variant="link">Link</Button>
+</>
     </p>
   </Layout>
 )
